158825ba455faf7e323b42c76f5329c513b08f5a
yves.albuquerque@gmail.com <yves.albuquerque@gmail.com@c59576b2-56fa-3ac8-f216-b10c56ae58d3>
2013-02-12 18:05:57 +0000

    Comecei a organizar os prefabs mas ainda falta um monte.

--- trunk/Assets/Scripts/LevelManager.js
+++ trunk/Assets/Scripts/LevelManager.js
@@ -1,14 +1,14 @@
 //Level Manager 31/1/2013
-//How to use: Put this code into your player prefab
-//What it does: Move the character to the sides when acelerates and apply gravity. Also constraits the character.
-//Last Modified: 31/1/2013
+//How to use: Put this code into a Game Manager Object
+//What it does: Creates a sequence and put some markers
+//Last Modified: 11/2/2013
 //by Yves J. Albuquerque
 
 #pragma strict
 
-var parts : GameObject[]; //Put here all cenario sequences
-var depth : float = 100; //Default cenario sequence depth
+var sequence : GameObject[]; //Put here all cenario sequences
 var placa : GameObject; //Marcador de Distancia
+private var depth : float = 100; //Default cenario sequence depth
 private var depthMultiplyer : float = 0; // Works like an index to depth control
 private var lastPart : GameObject; //Last Instantiated sequence
 
@@ -20,14 +20,9 @@ function Start ()
 	NewPart();
 }
 
-function Update ()
-{
-	
-}
-
 function NewPart ()
 {
-	lastPart = Instantiate (parts[Random.Range(0,parts.Length)],Vector3(0,0,depthMultiplyer*depth),Quaternion.identity);
+	lastPart = Instantiate (sequence[Random.Range(0,sequence.Length)],Vector3(0,0,depthMultiplyer*depth),Quaternion.identity);
 	depthMultiplyer ++;
 	transform.position.z = depth * (depthMultiplyer-1);
 }
